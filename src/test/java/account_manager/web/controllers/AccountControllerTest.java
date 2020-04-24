67014c7f688598a21862122c2e8d1be0448411bb
YuliiaV <ogirenkojulia@gmail.com>
2020-04-24 16:48:04 +0300

    Commit 32: refactor AccountControllerTest using SpringBoot

--- src/test/java/account_manager/web/controllers/AccountControllerTest.java
+++ src/test/java/account_manager/web/controllers/AccountControllerTest.java
@@ -1,56 +1,48 @@
 package account_manager.web.controllers;
 
+import account_manager.security.WebSecurityConfig;
 import account_manager.service.AccountService;
 import account_manager.service.ClientService;
 import account_manager.service.dto.AccountDto;
 import account_manager.service.dto.ClientDto;
-import account_manager.web.WebConfiguration;
 import account_manager.web.controller.AccountController;
-import account_manager.web.exception_handling.CustomExceptionHandler;
 import account_manager.web.exception_handling.InputParameterValidationException;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {AccountControllerTest.TestConfiguration.class})
-@WebAppConfiguration
+@WebMvcTest(controllers = AccountController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfig.class)
+        })
 public class AccountControllerTest {
+    @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
+    @MockBean
     private AccountService accountService;
-    @Autowired
-    private ClientService clientService;
-    @Autowired
-    private WebApplicationContext webAppContext;
 
-    @Before
-    public void setup() {
-        reset(accountService);
-        reset(clientService);
-        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
-    }
+    @MockBean
+    private ClientService clientService;
 
     private final String EXCEPTION_MESSAGE = "Exception message";
     private final String ERROR_DTO_JSON = "{\n"
@@ -82,6 +74,7 @@ public class AccountControllerTest {
             + "}";
 
     @Test
+    @WithMockUser
     public void getAccountById_AccountFound_ShouldReturnFoundAccount() throws Exception {
         when(accountService.getById(1)).thenReturn(accountDtoWithNotNullId);
         mockMvc.perform(get("/account?accountId=1"))
@@ -90,6 +83,7 @@ public class AccountControllerTest {
     }
 
     @Test
+    @WithMockUser
     public void getAccountById_AccountNotFound_ShouldReturnErrorDto() throws Exception {
         when(accountService.getById(1)).thenThrow(new InputParameterValidationException(EXCEPTION_MESSAGE));
         mockMvc.perform(get("/account?accountId=1"))
@@ -99,9 +93,11 @@ public class AccountControllerTest {
     }
 
     @Test
+    @WithMockUser
     public void createAccount_InvalidAccount_ShouldReturnErrorDto() throws Exception {
         when(accountService.create(accountDtoWithNotNullId)).thenThrow(new InputParameterValidationException(EXCEPTION_MESSAGE));
         mockMvc.perform(post("/account")
+                .with(csrf())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(ACCOUNT_DTO_WITH_NOT_NULL_ID_JSON))
                 .andExpect(status().isInternalServerError())
@@ -110,9 +106,11 @@ public class AccountControllerTest {
     }
 
     @Test
+    @WithMockUser
     public void createAccount_AccountIsValid_ShouldReturnSuccessMessage() throws Exception {
         when(accountService.create(accountDtoWithNullId)).thenReturn("Created account #1");
         MvcResult result = mockMvc.perform(post("/account")
+                .with(csrf())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(ACCOUNT_DTO_WITH_NULL_ID_JSON))
                 .andExpect(status().isOk())
@@ -122,9 +120,11 @@ public class AccountControllerTest {
     }
 
     @Test
+    @WithMockUser
     public void updateAccount_AccountIsValid_ShouldReturnSuccessMessage() throws Exception {
         when(accountService.update(accountDtoWithNotNullId)).thenReturn("Account updated successfully");
         MvcResult result = mockMvc.perform(put("/account")
+                .with(csrf())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(ACCOUNT_DTO_WITH_NOT_NULL_ID_JSON))
                 .andExpect(status().isOk())
@@ -134,9 +134,11 @@ public class AccountControllerTest {
     }
 
     @Test
+    @WithMockUser
     public void updateAccount_AccountIsInvalid_ShouldReturnErrorDto() throws Exception {
         when(accountService.update(accountDtoWithNullId)).thenThrow(new InputParameterValidationException(EXCEPTION_MESSAGE));
         mockMvc.perform(put("/account")
+                .with(csrf())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(ACCOUNT_DTO_WITH_NULL_ID_JSON))
                 .andExpect(status().isInternalServerError())
@@ -145,9 +147,11 @@ public class AccountControllerTest {
     }
 
     @Test
+    @WithMockUser
     public void deleteAccount_AccountFound_ShouldReturnSuccessMessage() throws Exception {
         when(accountService.deleteById(1)).thenReturn("Deleted account #1");
-        MvcResult result = mockMvc.perform(delete("/account?accountId=1"))
+        MvcResult result = mockMvc.perform(delete("/account?accountId=1")
+                .with(csrf()))
                 .andExpect(status().isOk())
                 .andReturn();
         String body = result.getResponse().getContentAsString();
@@ -155,6 +159,7 @@ public class AccountControllerTest {
     }
 
     @Test
+    @WithMockUser
     public void getByClientId_AccountsAndClientFound_ShouldRenderAccountsByClientView() throws Exception {
         List<AccountDto> accounts = Collections.singletonList(accountDtoWithNotNullId);
         when(clientService.getById(1)).thenReturn(client);
@@ -170,35 +175,12 @@ public class AccountControllerTest {
     }
 
     @Test
+    @WithMockUser
     public void getByClientId_ClientNotFound_ShouldReturnErrorDto() throws Exception {
         when(clientService.getById(1)).thenThrow(new InputParameterValidationException(EXCEPTION_MESSAGE));
         mockMvc.perform(get("/account/get-by-client?clientId=1"))
                 .andExpect(status().isInternalServerError())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().json(ERROR_DTO_JSON));
-    }
-
-    @Configuration
-    @Import(WebConfiguration.class)
-    public static class TestConfiguration {
-        @Bean
-        public AccountService accountService() {
-            return mock(AccountService.class);
-        }
-
-        @Bean
-        public ClientService clientService() {
-            return mock(ClientService.class);
-        }
-
-        @Bean
-        public AccountController accountController(ClientService clientService, AccountService accountService) {
-            return new AccountController(accountService, clientService);
-        }
-
-        @Bean
-        public CustomExceptionHandler customExceptionHandler() {
-            return new CustomExceptionHandler();
-        }
     }
 }
